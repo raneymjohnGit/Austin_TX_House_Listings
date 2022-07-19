de8756b4abc478726ef4b507d07f47d4512e0ebc
Raney Mathew John <raneymjohn@gmail.com>
2022-07-18 20:41:54 -0500

    Deleted README from raneybranch

---
+++ Raney/static/js/logic.js
@@ -0,0 +1,27 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+	center: [30.267153, -97.7430608],
+	zoom: 10,
+	layers: [streets]
+});
+console.log("working");
+
+
+
+// Then we add a control to the map that will allow the user to change which
+// layers are visible.
+//L.control.layers(baseMaps).addTo(map);
+
+
+
+
