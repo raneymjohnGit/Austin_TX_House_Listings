11538aed40f624937bf2b0dc88903ffa42a09ab6
Raney Mathew John <raneymjohn@gmail.com>
2022-07-21 22:32:30 -0500

    Added code for the website

--- Raney/app.py
+++ Raney/app.py
@@ -1,36 +1,55 @@
-from flask import Flask, render_template
+from xml.dom.minidom import Document
+from flask import Flask, render_template, request
 import predictions
-import jyserver.Flask as js
-from jyserver.Server import Client, Server
 
 app = Flask(__name__)
 
-@js.use(app)
-class App:
-    def __init__(self):
-        self.sqft = 2000
-        self.schrat = 7
-        self.bathcount = 3
-        self.houseprice = 0
-        self.count = 0
-    
-    def call_prediction(self):
-        #self.sqft = self.js.document.getElementById("sqft").value        
-        #self.schrat = self.js.document.getElementById("isd").value        
-        #self.bathcount = self.js.document.getElementById("bath").value      
-        #print("before call")
-        #print(str(self.sqft))
-        #self.houseprice = predictions.predict_houseprice(self.sqft, self.schrat, self.bathcount)
-        self.count += 1
-        self.js.dom.price.innerHTML = self.count
-        print('after call')
+@app.route('/')  
+def index_page():
+    return render_template('index.html')
 
-       
+@app.route('/results',methods=['GET','POST'])  
+def submit():
+    print('before')
+    errorMessage = ""    
+    sqftMessage = ""
+    isdMessage = ""
+    bathMessage = ""
+    priceMessage = ""
+    bedMessage = ""
+    if request.method == 'POST':                
+        try:
+            squarefeet = int(request.form['sqft'])
+            sqftMessage = f"Square Footage = {squarefeet}"
+        except:
+            if errorMessage == "":
+                errorMessage = "Square Feet needs to greater than 0"                                
+        try :
+            schoolrat = int(request.form['isd'])
+            isdMessage = f"School Rating = {schoolrat}"
+        except :
+            if errorMessage == "":
+                errorMessage = "School Rating needs to greater than 0"
+            else:    
+                errorMessage += " \n School Rating needs to greater than 0"
+        try:
+            numbeofbath = int(request.form['bath'])
+            bathMessage = f"Number of Baths = {numbeofbath}"
+        except:
+            if errorMessage == "":
+                errorMessage = "School Rating needs to greater than 0"
+            else:
+                errorMessage +=  "\n School Rating needs to greater than 0"
+        if errorMessage == "":
+            price = predictions.predict_houseprice(squarefeet, schoolrat, numbeofbath)
+            priceMessage = "Predicted Price = " + '${:,.2f}'.format(price)
 
-@app.route("/")
-def index():
-    return App.render(render_template("index.html"))
+            
 
+
+            return render_template('results.html', sqftMessage = sqftMessage, isdMessage= isdMessage, bathMessage= bathMessage,bedMessage = bedMessage,priceMessage = priceMessage)
+        else:
+            return render_template('index.html', errorMessage = errorMessage)
 
 if __name__ == "__main__":
    app.run()
--- Raney/predictions.py
+++ Raney/predictions.py
@@ -4,4 +4,4 @@ def predict_houseprice (sqft = 2000, schrat = 7, bathcount = 3):
     model = joblib.load("model.joblib")
     predictions = model.predict([[sqft,schrat,bathcount]])
     print("inside predict_house")
-    return predictions[0]
+    return int(predictions[0])
