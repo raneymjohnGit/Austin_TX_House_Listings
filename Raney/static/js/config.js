935b75dcc083071cdaf0756f61fc279705c38113
raneymjohnGit <99343521+raneymjohnGit@users.noreply.github.com>
2022-08-01 17:59:03 -0500

    Delete config.js

--- Raney/static/js/config.js
+++
@@ -1,3 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1IjoicmFuZXltam9obiIsImEiOiJjbDM5YjcxdzkwNDI5M2Nta3czZDVmY2d4In0.6sjElD0MUDFMftNJcegTJw";
-const JSON_URL = "https://finalproject-04.s3.amazonaws.com/dataset.json";
